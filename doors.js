c2c5bff5b9ec7bdd92fe755ca4a37240cda1bb44
Rasmus Graver <rasmus.graver@gmail.com>
2024-01-29 09:58:08 +0100

    Litt formattering - fjernet semi colons i prettier formatting

--- doors.js
+++ doors.js
@@ -1,12 +1,14 @@
 // Om vi bare vil la brukeren trykke én gang:
-let kanTrykke = true;
-const vinnerLuke = Math.floor(Math.random() * 3);
+let kanTrykke = true
+
+// Vinnertallet vårt:
+const vinnerLuke = Math.floor(Math.random() * 3)
 
 /* Finn alle våre dører og add event listener til dem */
-doors = document.querySelectorAll(".openclose");
+const doors = document.querySelectorAll(".openclose")
 for (let i = 0; i < doors.length; i++) {
-  const door = doors[i];
-  door.addEventListener("click", klikkdor);
+  const door = doors[i]
+  door.addEventListener("click", klikkdor)
 }
 /* Den "raske" måten å gjøre det på: 
   document.querySelectorAll('.openclose').forEach(item => {
@@ -15,41 +17,41 @@ for (let i = 0; i < doors.length; i++) {
 
 function klikkdor(event) {
   if (kanTrykke) {
-    const door = event.target;
+    // Finner døren som ble trykket ved hjelp av "event.target":
+    const door = event.target
     let vinner = false
 
     // la brukeren trykke en eller flere ganger: kanTrykke = false;
 
     // Hvilken dør er dette?
+    // Hadde denne lange måten å gjøre det på (som ikke trenger noen "data-index" property):
     // const door_index = Array.from(      door.parentElement.parentElement.children    ).indexOf(door.parentElement);
-    // Eller ved å bruke data-index property:
+    // Eller ved å bruke "data-index" property (som ligger på "parent elementet"):
     const door_index = door.parentElement.dataset.index
-    console.log("Dør nummer", door_index, "VinnerLuke", vinnerLuke);
+    console.log("Dør nummer", door_index, "VinnerLuke", vinnerLuke)
     if (door_index == vinnerLuke) {
       vinner = true
-      document.getElementById("outputText").innerHTML = "DU VANT! DU VANT!" 
+      document.getElementById("outputText").innerHTML = "DU VANT! DU VANT!"
       const prizeBilde = door.parentElement.querySelector(".prize")
       prizeBilde.src = "media/ferrari.png"
       prizeBilde.style.width = "80%"
-
     }
 
     // Åpne døra "litt"
-    door.src = "media/semiclosed.png";
-    setTimeout(opendoor, 800, door);
-    // Spill lyden med en gang 
-    let audio = null 
+    door.src = "media/semiclosed.png"
+    setTimeout(opendoor, 800, door)
+    // Spill lyden med en gang
+    let audio = null
     if (vinner) {
-      audio = new Audio("media/success.mp3");
+      audio = new Audio("media/success.mp3")
     } else {
-      audio = new Audio("media/goat.mp3");
+      audio = new Audio("media/goat.mp3")
     }
-    audio.play();
-
+    audio.play()
   }
 }
 
 function opendoor(door, vinner) {
   // Åpne døra helt
-  door.src = "media/open.png";
+  door.src = "media/open.png"
 }
